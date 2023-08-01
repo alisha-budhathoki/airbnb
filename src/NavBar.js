ed7d4f76599a2d21406fa3c0e4bc45e89bc079dc
Alisha <alisha@Alishas-MacBook-Pro.local>
2023-08-01 12:58:05 +0545

    addn navbar

---
+++ src/NavBar.js
@@ -0,0 +1,9 @@
+import logo from "./images/logo.png";
+function NavBar() {
+    return (
+        <nav className="nav">
+            <img src={logo} className="nav-logo" />
+        </nav>
+    )
+}
+export default NavBar;
